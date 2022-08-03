6da2c38996843724c66ac64ac2e93d85968bfb48
Kevin <kevinohiro@gmail.com>
2022-08-03 15:12:54 +0100

    feat: completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,41 @@
+import 'package:bloc_fetch_api/cubit/post_fetch_cubit.dart';
+import 'package:bloc_fetch_api/repository/api_repository.dart';
+import 'package:bloc_fetch_api/service/api_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'screens/home_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp(apiService: ApiService(),));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key, required this.apiService}) : super(key: key);
+
+
+  final ApiService apiService;
 
  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<PostFetchCubit> (create: (context) => PostFetchCubit(apiRepository: ApiRepository(
+      apiService: apiService,
+    ),)..fetchPostApi(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+
+          primarySwatch: Colors.purple,
+        ),
+
+        home: const MyHomePage(),
       ),
-      
-      home: const MyHomePage(),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:bloc_fetch_api/cubit/post_fetch_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 
@@ -9,8 +13,39 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: const Text("Fetch data from API with Cubit"),
       ),
+      body: BlocBuilder<PostFetchCubit, PostFetchState>(
+          builder: (context, state){
+         if(state is PostFetchStateLoading){
+           return   Align(
+             alignment: Alignment.center,
+               child: Platform.isAndroid ?
+               const CircularProgressIndicator() : const CircularProgressIndicator.adaptive());
+         } else if(state is PostFetchStateError){
+           return Text(state.failure.message);
+         } else if(state is PostFetchStateData){
+           final postList = state.postList;
+           if(postList.isNotEmpty){
+             return ListView.builder(
+                 shrinkWrap: true,
+                 itemCount: state.postList.length,
+                 itemBuilder: (context, index){
+                   return  ListTile(
+                     title: Text(postList[index].title),
+                   );
+                 }
+             );
+           } else {
+             return const Center(
+               child: Text("There are currently no posts available"),
+             );
+           }
+
+         } else {
+           return const SizedBox();
+         }
+      }),
     );
   }
 }
