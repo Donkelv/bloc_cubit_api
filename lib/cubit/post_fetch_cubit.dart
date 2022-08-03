0a3bc3b19747e0fa006d7d8f68ef8f4cd2531a2a
Kevin <kevinohiro@gmail.com>
2022-08-03 12:41:32 +0100

    feat: added data and error states

--- lib/cubit/post_fetch_cubit.dart
+++ lib/cubit/post_fetch_cubit.dart
@@ -1,7 +1,10 @@
 import 'dart:html';
 
+import 'package:bloc_fetch_api/models/failure_model.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../models/post_model.dart';
 
 part 'post_fetch_state.dart';
 
--- lib/cubit/post_fetch_state.dart
+++ lib/cubit/post_fetch_state.dart
@@ -4,13 +4,28 @@ abstract class PostFetchState extends Equatable {
   const PostFetchState();
   @override
   List<Object> get props => [];
-
 }
 
-class PostFetchStateInitial extends PostFetchState{}
+class PostFetchStateInitial extends PostFetchState {}
 
-class PostFetchStateLoading extends PostFetchState{}
+class PostFetchStateLoading extends PostFetchState {}
 
-class PostFetchStateData extends PostFetchState{}
+class PostFetchStateData extends PostFetchState {
+  final List<Post> postList;
 
-class PostFetchStateError extends PostFetchState{}
+  const PostFetchStateData({required this.postList});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [postList];
+}
+
+class PostFetchStateError extends PostFetchState {
+  final Failure failure;
+
+  const PostFetchStateError({required this.failure});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [failure];
+}
